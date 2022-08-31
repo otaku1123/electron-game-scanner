a02a4c1aff98a92af3f140346ce75674443b3710
Taku Ogawa <ogawtaku@gmail.com>
2022-09-01 01:15:43 +0900

    Initial commit

---
+++ src/renderer/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <Tutorial /> -->
+  <button @click="hello()">hello world</button>
+</template>
+
+<!-- <script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script> -->
+<script>
+export default {
+  methods: {
+    hello() {
+      // console.log('hello world')
+      window.api.hello()
+    }
+  },
+}
+</script>
